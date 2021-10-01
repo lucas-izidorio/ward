98589007031bfdb6c3ea494090dbd23108d8ae3c
RodrigoGR98 <roxdrigo1883@gmail.com>
2021-09-30 21:18:30 -0300

    Include images directory

--- extension/script.js
+++ extension/script.js
@@ -4,15 +4,43 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log("DOM carregado!");
 
     function modifyDOM() {
-      //Pega todas as imagens do DOM pela tag IMG
+
       var images = document.getElementsByTagName('img');
       var srcList = [];
       for (var i = 0; i < images.length; i++) {
-        srcList.push(images[i].src);
+        srcList.push(images[i]);
+      };
+
+      srcList.map(function (foto, index) {
+        sendJSON(foto.src, index);
+      });
+
+      //Muda o atributo alt da tag img
+      function changeAlt(caption, index) {
+        srcList[index].alt = caption;
       }
-      srcList.map(function (foto) {
-        console.log(foto);
-      })
+
+      //Função para request na API
+      function sendJSON(imageSrc, index) {
+        console.log(imageSrc, index);
+
+        var data = {
+          "url": imageSrc,
+          "language": "pt"
+        }
+
+        var json = JSON.stringify(data);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", 'https://7759-2804-7f2-2990-e53b-8d75-879d-e16a-efc5.ngrok.io/images/');
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onload = function () {
+          var jsonResponse = JSON.parse(xhr.responseText);
+          changeAlt(jsonResponse.caption, index);
+        };
+        xhr.send(json);
+      };
+
       return document.body.innerHTML;
     }
 
